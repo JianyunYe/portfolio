a5efeb1bd1998babea198edbbb2d17e0ec0b6d31
JianyunYe <Jiy099@ucsd.edu>
2025-01-25 22:43:36 -0800

    11
    11

--- global.js
+++ global.js
@@ -3,7 +3,7 @@ const pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
     { url: 'cv/', title: 'CV' },
-    { url: 'https://github.com/JianyunYe', title: 'GitHub Profile' }
+    { url: 'https://github.com/JianyunYe', title: 'GitHub Profile' },
   ];
   
   const ARE_WE_HOME = document.documentElement.classList.contains('home');
@@ -14,23 +14,39 @@ const pages = [
   for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    
+  
     if (!ARE_WE_HOME && !url.startsWith('http')) {
       url = '../' + url;
     }
-    
+  
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
-    
+  
     if (url.startsWith('http')) {
       a.target = "_blank";
     }
-    
+  
     a.classList.toggle(
       'current',
       a.host === location.host && a.pathname === location.pathname
     );
-    
+  
     nav.append(a);
   }
+  
+  const detectColorScheme = () =>
+    matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+  
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `<label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic (${detectColorScheme()})</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>`
+  );
+  
