eee20e5d29358b49b880aafb6b516d7042e830e9
JianyunYe <Jiy099@ucsd.edu>
2025-01-31 23:36:15 -0800

    lab 4
    test 15

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ import { fetchJSON, renderProjects } from '/portfolio/global.js';
         console.error('Fail to load the latest projects:', error);
     }
 })();
+
+import { fetchGitHubData } from '/portfolio/global.js';
+
+(async () => {
+    try {
+        const githubData = await fetchGitHubData("JianyunYe");
+        console.log("GitHub Data:", githubData);
+
+    } catch (error) {
+        console.error("Error fetching GitHub data:", error);
+    }
+})();
