7a6e9701ca174b2946cbc86e1d132523adaff6b7
JianyunYe <Jiy099@ucsd.edu>
2025-01-31 23:54:24 -0800

    final
    final

--- projects/projects.js
+++ projects/projects.js
@@ -2,13 +2,19 @@ import { fetchJSON, renderProjects } from '/portfolio/global.js';
 
 (async () => {
     try {
-        const projects = await fetchJSON('/portfolio/lib/projects.json');
-
         const projectsContainer = document.querySelector('.projects');
+        if (!projectsContainer) {
+            console.error("Error: .projects container not found.");
+            return;
+        }
+
+        projectsContainer.innerHTML = "";
+
+        const projects = await fetchJSON('/portfolio/lib/projects.json');
 
         renderProjects(projects, projectsContainer, 'h2');
 
     } catch (error) {
-        console.error('Error loading projects:', error);
+        console.error('Fail to load projects:', error);
     }
 })();
